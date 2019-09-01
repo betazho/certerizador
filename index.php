02043e71591f77e2d7216212f29cf96fe6e60d89
Berto <bertrevino@gmail.com>
2019-08-31 19:33:02 -0500

    Index.php
    Sólo un php para validar conexión con db

---
+++ index.php
@@ -0,0 +1,10 @@
+<?
+// Database hostname    = localhost:3306
+// Database name        = crt_
+// Database username    = crtadm
+// Database password    = 32Z~tlt6
+
+
+mysql_connect('localhost:3306', 'crtadm', '32Z~tlt6') or die('Could not connect the database : Username or password incorrect');
+mysql_select_db('crt_') or die ('No database found');
+echo 'Database Connected successfully';
