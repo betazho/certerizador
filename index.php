c021c5cbd50fc89d710d365daee3c327928ed4da
Berto <bertrevino@gmail.com>
2019-08-31 19:42:02 -0500

    fix
    fix

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?
-// Database hostname    = localhost:3306
-// Database name        = crt_
-// Database username    = crtadm
-// Database password    = 32Z~tlt6
-
-
+    /** 
+        *Database hostname    = localhost:3306
+        *Database name        = crt_
+        *Database username    = crtadm
+        *Database password    = 32Z~tlt6
+    */
+?>
+<html>
+<? 
 mysql_connect('localhost:3306', 'crtadm', '32Z~tlt6') or die('Could not connect the database : Username or password incorrect');
 mysql_select_db('crt_') or die ('No database found');
 echo 'Database Connected successfully';
+
+?>
+    <head>
+    </head>
+    <body>
+    </body>
+</html>
