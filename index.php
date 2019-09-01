5e1f38fcaf5d0078860ac8d8a00c5c475c50d642
Berto <bertrevino@gmail.com>
2019-08-31 20:22:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,25 @@
     */
 ?>
 <html>
-
     <head>
     </head>
     <body>
     <!-- Test -->
     Hola!
+
+<?php
+$servername   = "localhost:3306";
+$database = "crt_";
+$username = "crtadm";
+$password = "32Z~tlt6";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+// Check connection
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+  echo "Connected successfully";
+?>
     </body>
 </html>
