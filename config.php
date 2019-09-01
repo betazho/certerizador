e6584b01b91e07a408fb802e361ce2392d0a25ac
Berto <bertrevino@gmail.com>
2019-09-01 10:31:19 -0500

    Creación de config
    Creación de archivo config para la conexión a la base de datos y configuraciones posteriores del sistema

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+	* Archivo de configración del sistema y la conexión con la base de datos.
+	* Database hostname    = localhost:3306
+	* Database name        = crt_
+	* Database username    = crtadm
+	* Database password    = 32Z~tlt6
+*/
+	$servername   = "localhost:3306";
+	$database = "crt_";
+	$username = "crtadm";
+	$password = "32Z~tlt6";
+	$conn = new mysqli($servername, $username, $password, $database);
+	mysqli_set_charset( $conn, 'utf8' );
+
+
+	// Probar conexión
+	/*if ($conn->connect_error) {
+	   die("Connection failed: " . $conn->connect_error);
+	}
+	  echo "Connected successfully";*/
+?>
--- index.php
+++ index.php
@@ -1,10 +1,6 @@
-<?
-    /** 
-        *Database hostname    = localhost:3306
-        *Database name        = crt_
-        *Database username    = crtadm
-        *Database password    = 32Z~tlt6
-    */
+<?php
+    require_once('conne_ct.php');
+    date_default_timezone_set('America/Mexico_City');
 ?>
 <html>
     <head>
@@ -12,20 +8,5 @@
     <body>
     <!-- Test -->
     Hola!
-
-<?php
-$servername   = "localhost:3306";
-$database = "crt_";
-$username = "crtadm";
-$password = "32Z~tlt6";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-// Check connection
-if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
-}
-  echo "Connected successfully";
-?>
     </body>
 </html>
