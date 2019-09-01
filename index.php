f84c4ddc95184f503ad6470e1773ffa9148c88bb
Berto <bertrevino@gmail.com>
2019-08-31 20:20:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,7 @@
     */
 ?>
 <html>
-<? 
-mysql_connect('localhost:3306', 'crtadm', '32Z~tlt6') or die('Could not connect the database : Username or password incorrect');
-mysql_select_db('crt_') or die ('No database found');
-echo 'Database Connected successfully';
 
-?>
     <head>
     </head>
     <body>
